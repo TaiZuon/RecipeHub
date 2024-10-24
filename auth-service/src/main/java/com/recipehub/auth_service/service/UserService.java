563311bab016c06c4e4f48eb57bbd669f3b902b9
TaiZuon <124855937+TaiZuon@users.noreply.github.com>
2024-10-24 21:36:20 +0700

    init commit

---
+++ auth-service/src/main/java/com/recipehub/auth_service/service/UserService.java
@@ -0,0 +1,31 @@
+package com.recipehub.auth_service.service;
+
+import com.recipehub.auth_service.dto.request.UserCreationRequest;
+import com.recipehub.auth_service.entity.User;
+import com.recipehub.auth_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findUserById(String id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
